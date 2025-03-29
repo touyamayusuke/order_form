4b9b5450ab6a4327fc1092780908528011c411e2
touyamayusuke <yusuke2710@icloud.com>
2025-03-29 23:24:35 +0900

    Initialize

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,23 @@
+class OrdersController < ApplicationController
+  def new
+    @order = Order.new
+  end
+
+  def confirm
+    @order = Order.new(order_params)
+  end
+
+  def create
+    @order = Order.new(order_params)
+    return render :new if params[:button] == 'back'
+    return redirect_to complete_orders_url if @order.save
+
+    render :confirm
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:name)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'sidekiq/web'
+
+Rails.application.routes.draw do
+  resources :orders, only: %i[new create] do
+    collection do
+      post :confirm
+      get :complete
+    end
+  end
+  mount Sidekiq::Web, at: '/sidekiq'
+end
