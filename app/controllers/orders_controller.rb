134ceeda444a1d2293583c2e48aed6957beee3ec
touyamayusuke <yusuke2710@icloud.com>
2025-03-30 22:10:48 +0900

    finish create form

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,14 +10,29 @@ class OrdersController < ApplicationController
   def create
     @order = Order.new(order_params)
     return render :new if params[:button] == 'back'
-    return redirect_to complete_orders_url if @order.save
+    if @order.save
+      session[:order_id] = @order.id
+      return redirect_to complete_orders_url
+    end
 
     render :confirm
+  end
+
+  def complete
+    @order = Order.find_by(id: session[:order_id])
+    return redirect_to new_order_url if @order.blank?
+    
+    session[:order_id] = nil
   end
 
   private
 
   def order_params
-    params.require(:order).permit(:name)
+    params
+      .require(:order)
+      .permit(:name, 
+              :email, 
+              :telephone, 
+              :delivery_address)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_03_27_124518) do
+ActiveRecord::Schema[7.0].define(version: 2025_03_30_124039) do
   create_table "orders", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", null: false
+    t.string "telephone", null: false
+    t.string "delivery_address", null: false
   end
 
 end
